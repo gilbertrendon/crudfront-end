aab77f3a9c45804ac39d22ccf288c58b113e954a
gilbertrendon <jilberlv@gmail.com>
2020-10-31 17:39:06 -0500

    ajustes query

--- README.sql
+++ README.sql
@@ -3,8 +3,8 @@
 --http://localhost:8085/h2-console/login.jsp?jsessionid=353cab2a54c7e4ee3f4cd704b7388d53
 
 --para las inserciones desde la base de datos
-insert into car(id, aumento, finsercion, habilitado, img, valor)values (444, 0.05 , '28/10/2020', true, 'https://hips.hearstapps.com
-/hmg-prod.s3.amazonaws.com/images/ford-gt-subasta-1590059807.jpg?crop=0.985xw:0.911xh;0,0.0541xh&resize=480:*' ,200000 )
+insert into car(id, aumento, finsercion, habilitado, img, valor, modelo)values (444, 0.05 , '28/10/2020', true, 'https://hips.hearstapps.com
+/hmg-prod.s3.amazonaws.com/images/ford-gt-subasta-1590059807.jpg?crop=0.985xw:0.911xh;0,0.0541xh&resize=480:*' ,200000, '1980')
 
 --Rutas:
 --Obtener lista de carros: http://localhost:3000/api/cars
